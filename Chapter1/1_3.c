0080ab14c61e585847f8414b4f93658bc594b860
zhuye <zhuye@localhost.localdomain>
2014-12-28 22:17:14 +0800

    m

---
+++ Chapter1/1_3.c
@@ -0,0 +1,24 @@
+  /********************************************************
+  *   Copyright (C) 2014 All rights reserved.
+  *   
+  *   Filename:1_3.c
+  *   Author  :zhuye
+  *   Date    :2014-12-28
+  *   Describe:
+  *
+  ********************************************************/
+#include<apue.h>
+#include<dirent.h>
+
+int main (int argc,char *argv[]){
+    DIR *dp;
+    struct dirent *dirp;
+    if(argc != 2)
+        err_quit("usage: ls directory_name");
+    if((dp = opendir(argv[1])) == NULL)
+        err_sys("can't open %s",argv[1]);
+    while((dirp = readdir(dp)) != NULL)
+        printf("%s\n",dirp->d_name);
+    closedir(dp);
+    exit(0);
+}
