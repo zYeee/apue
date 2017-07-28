40a102210d17090215dcbb28f20e12770b5daf4c
zhuye <yformat930@126.com>
2017-07-28 01:32:08 +0000

    client and server

--- network/server.c
+++ network/server.c
@@ -1,12 +1,30 @@
-#include "stdio.h"
+#include"stdio.h"
+#include"string.h"
 #include <netdb.h>
-int open_clientfd(char *hostname, int port){
-  int clientfd;
-  struct hostent *hp;
-  hp = gethostbyname(hostname);
-  printf("%s", hp->h_name);
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int open_listen(int port){
+  int listenfd, optval = 1;
+  struct sockaddr_in serveraddr;
+
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)port);
+
+  bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+  listen(listenfd, 1024);
+  return listenfd;
 }
 int main(){
-  int a = open_clientfd("localhost", 80);
-
+  struct sockaddr_in clientaddr;
+  int listenfd = open_listen(930);
+  printf("%d\n", listenfd);
+  int clientlen = sizeof(clientaddr);
+  int connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+  printf("%d", connfd);
 }
