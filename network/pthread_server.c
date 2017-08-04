f5dbbfb4c6a94fe2858b30ce8f45de821658361d
zhuye <yformat930@126.com>
2017-08-04 01:25:17 +0000

    solve the rece problem for multi server

--- network/pthread_server.c
+++ network/pthread_server.c
@@ -4,21 +4,24 @@
 void *thread(void *vargp){
   int connfd = *((int *)vargp);
   pthread_detach(pthread_self());
+  free(vargp); //function of free only can free the malloc and calloc
   echo(connfd);
   close(connfd);
   return NULL;
 }
 
 int main() {
-  int listenfd, connfd, port;
+  int listenfd, port, *connfdp;
   struct sockaddr_in clientaddr;
   int clientlen = sizeof(clientaddr);
   pthread_t tid;
 
   listenfd = open_listen(9301);
   while (1){
-    // will have rece problem
-    connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
-    pthread_create(&tid, NULL, thread, &connfd);
+    /* will have rece problem
+    int connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);*/
+    connfdp = malloc(sizeof(int));
+    *connfdp = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+    pthread_create(&tid, NULL, thread, connfdp);
   }
 }
